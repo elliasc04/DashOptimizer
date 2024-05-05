d6bd50b91a57b79c5be3d6e9cbf6c014bc62a692
elliasc04 <guoyethan@gmail.com>
2024-05-05 00:02:18 -0500

    plotting + solve pointer issue

--- 313project.py
+++ 313project.py
@@ -1,12 +1,9 @@
-from haversine import haversine, Unit
+from haversine import haversine
 from collections import deque
-import heapq
-from math import radians, inf
-from statistics import mean, median
-import csv, json
-import numpy as np
-import pandas as pd
-
+from math import inf
+from statistics import mean
+import csv, copy
+import matplotlib.pyplot as plt
 #INITIALIZATION/DATA READING===================================================================
 
 # Delivery ID, Order Created at, Food Ready Time, Pickup Lat, Pickup Long, Dropoff Lat, Dropoff Long
@@ -14,8 +11,8 @@ deliverydata_file = open("./IEMS313_ProjectData.csv", mode='r', encoding='utf-8'
 deliverydata = list(csv.reader(deliverydata_file))[1:]
 
 #Dasher ID, Dasher Lat, Dasher Long
-location_file = open("./DasherLocations.csv", mode='r', encoding='utf-8', errors='ignore')
-locationdata = list(csv.reader(location_file))[1:]
+dasher_file = open("./DasherLocations.csv", mode='r', encoding='utf-8', errors='ignore')
+dasherdata = list(csv.reader(dasher_file))[1:]
 
 
 
@@ -36,14 +33,13 @@ for delivery in deliverydata:
     delivery[5] = float(delivery[5])
     delivery[6] = float(delivery[6])
 #same thing
-for location in locationdata:
-    location[0] = int(location[0])
-    location[1] = float(location[1])
-    location[2] = float(location[2])
-
-#PROBLEM ONE========================================================================
+for person in dasherdata:
+    person[0] = int(person[0])
+    person[1] = float(person[1])
+    person[2] = float(person[2])
 
 
+# UTILITY FUNCTIONS
 #finds nearest dasher in list of dashers to order pickup, and returns the dasher, the dasher's new availability time (time when next available), and list index of dasher
 def findNearest(order:list, available: list):
     orderlat, orderlon = order[3], order[4]
@@ -77,6 +73,7 @@ def findNearest(order:list, available: list):
 
     #return the best dasher's info, when they will be next available, their list index so we can modify their info, and the total time from order creation to delivery
     return [bestdasher, dasheravailabletime, index, dasheravailabletime - order[1], droplat, droplon]
+
 
 
 #This dont work yet its assigning the same dasher 200 times to be the optimal dasher for some reason
@@ -120,13 +117,34 @@ def findOptimalNaive(order: list, available: list):
 def calcTime(distance: float):
     return ((distance*1000)/4.5)/60
 
-def methodOne(deliveryinfo: list[list], dashers: list[list]):
+#checks the average time it takes to travel from pickup to dropoff, 155.602 minutes.
+def findAverageDeliveryTime():
+    deliverytime = []
+    for delivery in deliverydata:
+        deliverytime.append(calcTime(haversine((delivery[3],delivery[4]),(delivery[5],delivery[6]))) + delivery[2]-delivery[1])
+    print(mean(deliverytime))
+
+
+#PROBLEM ONE========================================================================
+
+
+
+
+def methodOne(deliveryinfolist: list[list], dasherslist: list[list]):
+
+    dashers = copy.deepcopy(dasherslist)
+    deliveryinfo = copy.deepcopy(deliveryinfolist)
+
+    #initialize arrays to hold delivery durations and delivery dropoff times
     deliverydurations = []
+    endingtimes = []
+
     #sort delivery orders by time created 
     createddeliveryinfo = deque(sorted(deliveryinfo, key=lambda x: x[1]))
 
     #add extra field to track how many orders each dasher has
     for dasher in dashers:
+
         #let index 3 represent the amount of orders they currently have
         dasher.append(0)
 
@@ -144,23 +162,120 @@ def methodOne(deliveryinfo: list[list], dashers: list[list]):
 
         #get all available dashers at time of order creation
         availabledashers = [dasher for dasher in dashers if dasher[4] <= order[1]]
-        if not availabledashers:
-            availabledashers = dashers
-        nearestdasher, nextavailable, ind, ordertraveltime, droplat, droplon = findNearest(order, availabledashers)
+
+        #find the nearest dasher. If there are no available dashers, plug in all dashers. 
+        if availabledashers:
+            nearestdasher, nextavailable, ind, ordertraveltime, droplat, droplon = findNearest(order, availabledashers)
+        else:
+            nearestdasher, nextavailable, ind, ordertraveltime, droplat, droplon = findOptimalNaive(order, dashers)
 
         #track the order's delivery time
         deliverydurations.append(ordertraveltime)
+        endingtimes.append(nextavailable)
 
         #update the dasher's number of deliveries, when they will be next available, and their new location after dropping off
         dashers[ind][1] = droplat
         dashers[ind][2] = droplon
         dashers[ind][3] += 1
         dashers[ind][4] = nextavailable
-    print("mean:================")
+    
+    #calculate time from first order creation to last delivery dropoff in minutes
+    mindeliverytime = min([delivery[1] for delivery in deliverydata])
+    maxdeliverytime = max(endingtimes)
+    totalruntime = maxdeliverytime - mindeliverytime
+    
+    print("Average Delivery Duration:================")
     print(mean(deliverydurations))
 
+    print("Average Deliveries per Hour:==============")
+    print(len(deliverydata)/(totalruntime/60))
 
-# getUsableVals(deliverydata)
-# print([delivery[1] for delivery in deliverydata])
+    print("Longest Delivery Duration:================")
+    print(max(deliverydurations))
+
+
+
+
+
+#PROBLEM TWO:==============================================================
+
+
+def methodTwo(deliveryinfolist: list[list], dasherslist: list[list]):
+
+    dashers = copy.deepcopy(dasherslist)
+    deliveryinfo = copy.deepcopy(deliveryinfolist)
+    
+    #initialize arrays to hold delivery durations, deliveries made(by who), and delivery dropoff times
+    deliverydurations = []
+    endingtimes = []
+
+    #sort delivery orders by time created 
+    createddeliveryinfo = deque(sorted(deliveryinfo, key=lambda x: x[1]))
+
+    #add extra field to track how many orders each dasher has
+    for dasher in dashers:
+        #let index 3 represent the amount of orders they currently have
+        dasher.append(0)
+
+        #let index 4 represent the time at which they are done with all current orders
+        dasher.append(0)
+
+    #assign orders to dasher
+    while createddeliveryinfo:
+
+        #get next order and delete from sorted list of orders
+        order = createddeliveryinfo.popleft()
+
+        #get all available dashers at time of order creation
+        availabledashers = [dasher for dasher in dashers if dasher[4] <= order[1]]
+
+        #find the nearest dasher. If there are no available dashers, plug in all dashers. 
+        if availabledashers:
+            nearestdasher, nextavailable, ind, ordertraveltime, droplat, droplon = findNearest(order, availabledashers)
+        else:
+            nearestdasher, nextavailable, ind, ordertraveltime, droplat, droplon = findOptimalNaive(order, dashers)
+
+        #track the order's delivery time
+        deliverydurations.append(ordertraveltime)
+        endingtimes.append(nextavailable)
+
+        #update the dasher's number of deliveries, when they will be next available, and their new location after dropping off
+        dashers[ind][1] = droplat
+        dashers[ind][2] = droplon
+        dashers[ind][3] += 1
+        dashers[ind][4] = nextavailable
+    
+    dasherIDs = [dasher[0] for dasher in dashers]
+    dasherfrequencies = [dasher[3] for dasher in dashers]
+
+    #calculate time from first order creation to last delivery dropoff in minutes
+    mindeliverytime = min([delivery[1] for delivery in deliverydata])
+    maxdeliverytime = max(endingtimes)
+    totalruntime = maxdeliverytime - mindeliverytime
+    
+    print("Average Delivery Duration:================")
+    print(mean(deliverydurations))
+
+    print("Average Deliveries per Hour:==============")
+    print(len(deliverydata)/(totalruntime/60))
+
+    print("Longest Delivery Duration:================")
+    print(max(deliverydurations))
+
+    tick_label = [f"{dasherid}" for dasherid in dasherIDs]
+    plt.bar(dasherIDs, dasherfrequencies, tick_label = tick_label,
+        width = 0.8,)
+    plt.show()
+
+    print(len(deliverydurations))
+
+
+print("METHOD ONE:\n")
+methodOne(deliverydata, dasherdata)
+
+
+print("METHOD TWO:\n")
+methodTwo(deliverydata, dasherdata)
+
 
 print("done")
